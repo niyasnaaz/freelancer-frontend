f743bd5b49041b1f2991a10b99cfe32d9142a639
niyasnaaz <147026777+niyasnaaz@users.noreply.github.com>
2024-05-07 18:47:54 +0530

    Add files via upload

---
+++ freelancer-backend/controllers/order.controller.js
@@ -0,0 +1,75 @@
+import createError from "../utils/createError.js";
+import Order from "../models/order.model.js";
+import Gig from "../models/gig.model.js";
+import Stripe from "stripe";
+
+
+export const intent = async (req, res, next) => {
+  const stripe = new Stripe(process.env.STRIPE);
+
+  const gig = await Gig.findById(req.params.id);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    description: 'Software development services',
+    shipping: {
+      name: req.userId,
+      address: {
+        line1: '510 Townsend St',
+        postal_code: '98140',
+        city: 'San Francisco',
+        state: 'CA',
+        country: 'US',
+      },
+    },
+    amount: gig.price*100,
+    currency: 'usd',
+    payment_method_types: ['card'],
+  });
+
+  const newOrder = new Order({
+    gigId: gig._id,
+    img: gig.cover,
+    title: gig.title,
+    buyerId: req.userId,
+    sellerId: gig.userId,
+    price: gig.price,
+    payment_intent: paymentIntent.id,
+  });
+
+  await newOrder.save();
+
+  res.status(200).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+};
+
+export const getOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find({
+      ...(req.isSeller ? { sellerId: req.userId } : { buyerId: req.userId }),
+      isCompleted: true,
+    });
+
+    res.status(200).send(orders);
+  } catch (err) {
+    next(err);
+  }
+};
+export const confirm = async (req, res, next) => {
+  try {
+    const orders = await Order.findOneAndUpdate(
+      {
+        payment_intent: req.body.payment_intent,
+      },
+      {
+        $set: {
+          isCompleted: true,
+        },
+      }
+    );
+
+    res.status(200).send("Order has been confirmed.");
+  } catch (err) {
+    next(err);
+  }
+};
